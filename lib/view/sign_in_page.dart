caf62c641952e798f779432429c67cf20d1253b8
ToukaAlbaali <toukaalbaali@gmail.com>
2025-01-08 16:07:08 -0800

    add two page

--- lib/view/sign_in_page.dart
+++ lib/view/sign_in_page.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
